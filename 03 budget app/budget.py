fa6f0107457878b1e59d616e3dc1c90d3415d452
eLuizaTsuda <eLuizaTsuda@gmail.com>
2022-03-31 18:22:56 -0230

    category class and budget output

--- 03 budget app/budget.py
+++ 03 budget app/budget.py
@@ -1,7 +1,65 @@
 class Category:
+    def __init__(self, nameCategory):
+        self.category = nameCategory
+        self.ledger = []
+    
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        if not self.check_funds(amount): 
+            return False
+        else: 
+            self.ledger.append({"amount": amount * (-1), "description": description})
+            return True
+    
+    def get_balance(self):
+        currentBalance = 0
+        for event in self.ledger:
+            currentBalance += event["amount"]
+        return currentBalance
+
+    def transfer(self, amount, budgetCategory):
+        if not self.withdraw(amount, f"Transfer to {budgetCategory.category}"):
+            return False
+        else:
+            budgetCategory.deposit(amount, f"Transfer from {self.category}")
+            return True
+
+    def check_funds(self, amount): 
+        if self.get_balance() < amount:
+            return False
+        else:
+            return True
 
 
+    def __repr__(self):
+        # print(self.ledger)
+        # [
+        #   {'amount': 1000, 'description': 'initial deposit'}, 
+        #   {'amount': -10.15, 'description': 'groceries'}, 
+        #   {'amount': -15.89, 'description': 'restaurant and more food for dessert'}, 
+        #   {'amount': -50, 'description': 'Transfer to Clothing'}
+        # ]
 
+
+        num_star = (30 - len(self.category)) // 2
+        line_result = ""
+        line_header = ""
+        
+        line_result = line_header.ljust(num_star, "*") + self.category + line_header.rjust(num_star, "*") + "\n"
+
+        for item in self.ledger:
+            amount = "{:.2f}".format(item["amount"])
+            num_space = 30 - (len(item["description"][0:23]))
+
+            line_result += item["description"][0:23] + str(amount).rjust(num_space, " ") + "\n"
+        
+        totBalance = "{:.2f}".format(self.get_balance())
+        line_result += f"Total: {totBalance}"
+
+        return(line_result)
 
 
 def create_spend_chart(categories):
+    pass
--- 03 budget app/main.py
+++ 03 budget app/main.py
@@ -10,16 +10,17 @@ food.withdraw(15.89, "restaurant and more food for dessert")
 print(food.get_balance())
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
+clothing.withdraw(250.55, "jacket")
+clothing.withdraw(10, "shoes")
 auto = budget.Category("Auto")
 auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+auto.withdraw(15.500, "gas")
 
 print(food)
 print(clothing)
+print(auto)
 
-print(create_spend_chart([food, clothing, auto]))
+#print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
-#main(module='test_module', exit=False)
+#main(module='test_module', exit=False)import budget
