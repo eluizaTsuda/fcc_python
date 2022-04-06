d5e6c5a43e9aa5943e401bd44d41a98cfd3eb74b
eLuizaTsuda <eLuizaTsuda@gmail.com>
2022-04-06 16:25:01 -0230

    methods

--- 04 polygon-area-calculator/shape_calculator.py
+++ 04 polygon-area-calculator/shape_calculator.py
@@ -1,6 +1,34 @@
 class Rectangle:
 
+  def __init__(self, width, height):
+    self.width = width  
+    self.height = height
+
+    def set_width(self):
+      return(self.width)
+
+    def set_height(self):
+      return(self.height)
+
+    def get_area(self):
+      return(self.width * self.height)
+
+    def get_perimeter(self):
+      return((2 * self.width) + (2 * self.height))
+
+    def get_diagonal(self):
+      return(((self.width ** 2) + (self.height ** 2)) ** .5)
+
+    def get_picture(self):
+      pass
+
+    def get_amount_inside(self):
+      pass
+      
 
 
+class Square(Rectangle):
 
-class Square:
+    def set_side(self, side):
+      self.width = side
+      self.height = side
