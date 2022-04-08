c296501df58dffe5e7867f8b651d4a1fef3942b6
eLuizaTsuda <eLuizaTsuda@gmail.com>
2022-04-08 15:26:15 -0230

    get_picture and get_amount_inside

--- 04 polygon-area-calculator/shape_calculator.py
+++ 04 polygon-area-calculator/shape_calculator.py
@@ -3,57 +3,43 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width  
         self.height = height
-        #print("================== __init__")
-        #print(self.width, self.height)
-
+        
     def set_width(self, width):
-        #print("================== set_width")
-        #print(width)
         self.width = width
         return(self.width)
 
     def set_height(self, height):
-        #print("================== set_height")
-        #print(height)
         self.height = height
         return(self.height)
 
     def get_area(self):
-        #print("================== get_area")
-        #print(self.width * self.height)
         return(self.width * self.height)
 
     def get_perimeter(self):
-        #print("================== get_perimeter")
-        #print((2 * self.width) + (2 * self.height))
         return((2 * self.width) + (2 * self.height))
 
     def get_diagonal(self):
-        #print("================== get_diagonal")
-        #print(((self.width ** 2) + (self.height ** 2)) ** .5)
         return(((self.width ** 2) + (self.height ** 2)) ** .5)
 
     def get_picture(self):
-        #print("================== get_picture")
-        #print(self)
-
         if self.width > 50 or self.height > 50:
             return (f"Too big for picture.")
 
-        s_shape = ""
-        for row in range(0, self.height):
-            for col in range(0, self.width): 
-                s_shape += "*"
-            s_shape += "\n"
+        #s_shape = ""
+        #for row in range(0, self.height):
+        #    for col in range(0, self.width): 
+        #        s_shape += "*"
+        #    s_shape += "\n"
+        #
+        # or only one line (("*" * self.width) + "\n") * self.height
+
+        s_shape = (("*" * self.width) + "\n") * self.height
         return(s_shape)
         
-    
-
     def get_amount_inside(self, shape):
-      print(f"==============get_amount_inside shape= {shape}")
-      print(f"==============get_amount_inside self= {self}")
+      return(int(self.get_area() / shape.get_area()))
       
-    def __repr__(self):
+    def __str__(self):
         return(f"Rectangle(width={self.width}, height={self.height})")
 
 class Square(Rectangle):
@@ -61,13 +47,18 @@ class Square(Rectangle):
     def __init__(self, side):
         self.width = side  
         self.height = side
-        self.side = side
 
     def set_side(self, side):
-        self.side = side
         self.width = side  
         self.height = side
 
+    def set_width(self, side):
+        self.width = side  
+        self.height = side
 
-    def __repr__(self):
-        return(f"Square(side={self.side})")
+    def set_height(self, side):
+        self.width = side  
+        self.height = side
+    
+    def __str__(self):
+        return(f"Square(side={self.width})")
