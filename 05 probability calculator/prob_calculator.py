c438e699775123e58df31f262e9596516ecced5c
eLuizaTsuda <eLuizaTsuda@gmail.com>
2022-04-11 12:26:38 -0230

    class hat

--- 05 probability calculator/prob_calculator.py
+++ 05 probability calculator/prob_calculator.py
@@ -3,6 +3,14 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **balls_hat):
+        self.contents = []
+        for key, value in balls_hat.items():
+            print ("%s == %s" %(key, value))
+            for i in range(0, int(value)):
+                self.contents.append(key)
+        print(self.contents)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
